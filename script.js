24fd7a192f57c159836c1af6c3c022fcff5c373e
mw <47790981+mw-official@users.noreply.github.com>
2022-05-08 01:02:10 +0200

    Init

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello!')
+const iframe = document.getElementById("description");
+iframe.src =
+    "https://pl.wikipedia.org/wiki/Województwo_zachodniopomorskie";
+
+document.querySelector(".map").addEventListener("click", (ev) => {
+    if (ev.target.matches("a[href]")) {
+        ev.preventDefault();
+        window.location.hash = `#${ev.target.innerText}`;
+        iframe.src = ev.target.getAttribute("href");
+    }
+});
